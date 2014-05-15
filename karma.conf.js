ddb86082968a23cd98ead1eaa6731c91e6c5bdcc
Eric Lobdell <eric@ericlobdell.io>
2014-05-15 19:27:59 -0400

    Added more tests for List type. Got karma-coverage working.
    Signed-off-by: Eric Lobdell <eric@ericlobdell.io>

--- karma.conf.js
+++ karma.conf.js
@@ -1,49 +1,68 @@
 // Karma configuration
 // http://karma-runner.github.io/0.10/config/configuration-file.html
 
-module.exports = function(config) {
-  config.set({
-    // base path, that will be used to resolve files and exclude
-    basePath: '',
+module.exports = function (config) {
+    config.set({
+            // base path, that will be used to resolve files and exclude
+            basePath: '',
 
-    // testing framework to use (jasmine/mocha/qunit/...)
-    frameworks: ['jasmine'],
+            plugins: [
+                'karma-jasmine',
+                'karma-coverage',
+                'karma-chrome-launcher',
+                'karma-firefox-launcher',
+                'karma-safari-launcher'
+            ],
+            // testing framework to use (jasmine/mocha/qunit/...)
+            frameworks: ['jasmine'],
 
-    // list of files / patterns to load in the browser
-    files: [
-      'src/**/*.js',
-      'tests/*.js'
-    ],
-
-    reporters: ['dots'],
-    // list of files / patterns to exclude
-    exclude: [],
-
-    // web server port
-    port: 8080,
-
-    // level of logging
-    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-    logLevel: config.LOG_INFO,
+            // list of files / patterns to load in the browser
+            files: [
+                'src/**/*.js',
+                'tests/*.js'
+            ],
 
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+            coverageReporter: {
+                type: 'html',
+                dir: 'coverage/'
+            },
+
+            reporters: ['dots', 'coverage'],
+            preprocessors: {
+                'src/**/*.js': ['coverage']
+            },
+            // list of files / patterns to exclude
+            exclude: [],
+
+            // web server port
+            port: 8080,
+
+            // level of logging
+            // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+            logLevel: config.LOG_INFO,
 
 
-    // Start these browsers, currently available:
-    // - Chrome
-    // - ChromeCanary
-    // - Firefox
-    // - Opera
-    // - Safari (only Mac)
-    // - PhantomJS
-    // - IE (only Windows)
-    browsers: ['Chrome'],
+            // enable / disable watching file and executing tests whenever any file changes
+            autoWatch: true,
 
 
-    // Continuous Integration mode
-    // if true, it capture browsers, run tests and exit
-    singleRun: false
-  });
-};
+            // Start these browsers, currently available:
+            // - Chrome
+            // - ChromeCanary
+            // - Firefox
+            // - Opera
+            // - Safari (only Mac)
+            // - PhantomJS
+            // - IE (only Windows)
+            browsers: ['Chrome', 'Safari', 'Firefox'],
+
+
+            // Continuous Integration mode
+            // if true, it capture browsers, run tests and exit
+            singleRun: false
+        }
+    )
+    ;
+}
+;
--- tests/lists.spec.js
+++ tests/lists.spec.js
@@ -15,8 +15,47 @@ describe("List", function () {
         expect(list.clear).toBeDefined();
     });
 
+    describe("clear", function () {
+        beforeEach(function () {
+            list = new List();
+            list.dataStore = [1,2,3,4];
+        });
+        it("should set the dataStore back to an empty array", function () {
+            list.clear();
+            expect(list.dataStore instanceof Array).toBe(true)
+            expect(list.dataStore.length).toBe(0);
+        });
+
+        it("should set the current position to zero", function () {
+            list.pos = 2;
+
+            list.clear();
+            expect(list.pos).toBe(0);
+        });
+    });
+
     it("should have a find function to get the position of an element in the list", function () {
         expect(list.find).toBeDefined();
+    });
+
+    describe("find", function () {
+        beforeEach(function () {
+            list = new List();
+            list.dataStore = [1,2,3,4];
+        });
+        it("should return the position in the list of the element provided", function () {
+            var sut = list.find(2),
+                expected = 1;
+
+            expect(sut).toBe(expected);
+        });
+
+        it("should return the -1 if the element provided is not in the list", function () {
+            var sut = list.find(20),
+                expected = -1;
+
+            expect(sut).toBe(expected);
+        });
     });
 
     it("should have a toString function to enable seeing it's contents", function () {
@@ -25,6 +64,57 @@ describe("List", function () {
 
     it("should have an insert function to allow inserting an element at a certain position", function () {
         expect(list.insert).toBeDefined();
+    });
+
+    describe("insert", function () {
+        var after, newElement;
+        beforeEach(function () {
+            list = new List();
+             after = 3;
+             newElement = 100;
+            list.dataStore = [1,2,3,4];
+        });
+        it("should call find to locate the supplied element, to insert the new element after", function () {
+            spyOn(list, "find");
+
+            list.insert(newElement, after);
+
+            expect(list.find).toHaveBeenCalledWith(after);
+
+        });
+
+        it("should insert the new element in the position after the supplied element", function () {
+            var expectedPosition = 3;
+
+            list.insert(newElement, after);
+            var sut = list.find(newElement);
+
+            expect(sut).toBe(expectedPosition);
+        });
+
+        it("should return true if the element to insert after already exists in the list", function () {
+            var existsInList = 1;
+
+            var sut = list.insert(newElement, existsInList);
+
+            expect(sut).toBe(true);
+        });
+
+        it("should return false if the element to insert after doesn't exists in the list", function () {
+            var doesNotExistInList = 10;
+
+            var sut = list.insert(newElement, doesNotExistInList);
+
+            expect(sut).toBe(false);
+        });
+
+        it("should increment the listSize property", function () {
+            expect(list.listSize).toBe(0);
+
+            list.insert(newElement, after);
+
+            expect(list.listSize).toBe(1);
+        });
     });
 
     it("should have an append function to add an element to th end of the list", function () {
